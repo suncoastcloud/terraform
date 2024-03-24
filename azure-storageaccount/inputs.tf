df0d516d5dea8e03ba1a78997c0ed0aa1925ceb0
suncoastcloud <ken.pinto@outlook.com>
2024-03-23 22:12:45 -0400

    update files

---
+++ azure-storageaccount/inputs.tf
@@ -0,0 +1,4 @@
+variable "resource_group_location" {
+  default = "southcentralus"
+}
+
---
+++ azure-storageaccount/main.tf
@@ -0,0 +1,49 @@
+# Providers
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.97.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# /Providers
+
+
+# Random resource group name
+resource "random_pet" "rg_name" {
+  prefix = "scc"
+}
+
+# Random storage account name
+
+resource "random_id" "storage_account" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = random_pet.rg_name.id
+  location = var.resource_group_location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "scc${lower(random_id.storage_account.hex)}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
