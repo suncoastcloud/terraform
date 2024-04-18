743bd85db73a7f512d51668ad1d977c4c08932b9
suncoastcloud <ken.pinto@outlook.com>
2024-04-18 11:41:21 -0400

    update files

--- azure-automationaccount/main.tf
+++ azure-automationaccount/main.tf
@@ -1,15 +1,10 @@
-resource "random_pet" "my_pet" {
-  prefix = "scc"
-  length = 2
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = random_pet.my_pet.id
+  name     = "automation-rg"
   location = var.location
 }
 
 resource "azurerm_automation_account" "rg" {
-  name                = random_pet.my_pet.id
+  name                = "automation"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "Basic"
--- azure-automationaccount/providers.tf
+++ azure-automationaccount/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.99.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.6.1"
-    }
   }
 }
 
