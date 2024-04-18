53c4420fcb83a0bedac5afa7053fb44ce0858a7b
suncoastcloud <ken.pinto@outlook.com>
2024-04-18 15:22:25 -0400

    update files

---
+++ azure-automationaccount/azaccountshow.ps1
@@ -0,0 +1,3 @@
+az login --service-principal -u "1234" -p "1234" --tenant "1234"
+
+az account show
--- azure-automationaccount/main.tf
+++ azure-automationaccount/main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_automation_account" "rg" {
+resource "azurerm_automation_account" "aa" {
   name                = "automation"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -16,4 +16,27 @@ resource "azurerm_automation_account" "rg" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+resource "azurerm_role_assignment" "assign_contributor" {
+  principal_id         = azurerm_automation_account.aa.identity[0].principal_id
+  role_definition_name = "Contributor"
+  scope                = azurerm_automation_account.aa.id
+}
+
+data "local_file" "example" {
+  filename = "${path.module}/azaccountshow.ps1"
+}
+
+resource "azurerm_automation_runbook" "rb" {
+  name                    = "AZAccountShow"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.aa.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "This is an example runbook"
+  runbook_type            = "PowerShell72"
+
+  content = data.local_file.example.content
 }
