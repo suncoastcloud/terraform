36da9979845c4a7ac73e1f3799282b59f15ea8ec
suncoastcloud <ken.pinto@outlook.com>
2024-05-12 15:34:32 -0400

    update module

--- modules/azure-network/inputs.tf
+++ modules/azure-network/inputs.tf
@@ -1,23 +1,39 @@
 variable "vnet_name" {
   description = "Name of the virtual network"
-  type        = string
+  type        = list(string)
   default = "myVNet"
 }
+variable "subnet_names" {
+  description = "Names of the subnets"
+  type        = list(string)
+  default     = "subnet1"
+}
 
+variable "subnet_address_prefixes" {
+  description = "Address prefixes of the subnets"
+  type        = list(string)
+  default     = "10.0.1.0/24"
+}
+
+variable "subnet_nsgs" {
+  description = "Network Security Groups associated with the subnets"
+  type        = list(string)
+  default     = "nsg1"
+}
 variable "vnet_address_space" {
   description = "Address space of the virtual network"
   type        = list(string)
-  default     = ["10.0.0.0/16"]
+  default     = "10.0.0.0/16"
 }
 
 variable "vnet_location" {
   description = "Location of the virtual network"
-  type        = string
+  type        = list(string)
   default     = "southcentralus"
 }
 
 variable "resource_group_name" {
   description = "Name of the resource group"
-  type        = string
+  type        = list(string)
   default = "myResourceGroup"
 }
