f33e3a61c574d01e990af1a27cc704a673f9f813
suncoastcloud <ken.pinto@outlook.com>
2024-05-11 20:43:13 -0400

    update module files

--- azure-vnet/main.tf
+++ azure-vnet/main.tf
@@ -1,7 +1,7 @@
 // calls vnet module from a public repository
 
 module "vnet_module" {
-    source = "github.com/suncoastcloud/terraform//modules/vnet"
+    source = "github.com/suncoastcloud/terraform//modules/azure-network"
     vnet_name = "vnet_module"
     vnet_address_space = ["10.0.0.0/16"]
     vnet_location = "southcentralus"
