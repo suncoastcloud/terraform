602705e367cfdf84c577d6c47ce0388a90c89dfd
suncoastcloud <ken.pinto@outlook.com>
2024-05-12 15:55:25 -0400

    update module

--- modules/azure-network/network.tf
+++ modules/azure-network/network.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vnet" {
-    name                = var.vnet_name
-    address_space       = var.vnet_address_space
-    location            = var.vnet_location
+    name                = var.name
+    address_space       = var.address_space
+    location            = var.location
     resource_group_name = var.resource_group_name
 }
 
