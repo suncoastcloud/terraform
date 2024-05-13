23e79867d7b57f14cd2357945500ae99cea853dc
suncoastcloud <ken.pinto@outlook.com>
2024-05-13 10:29:16 -0400

    update module

--- modules/azure-network/network.tf
+++ modules/azure-network/network.tf
@@ -1,7 +1,8 @@
 resource "azurerm_virtual_network" "vnet" {
-    name                = var.name
+    name                = var.vnet_name
     address_space       = var.address_space
     location            = var.location
     resource_group_name = var.resource_group_name
 }
 
+//add subnet next
