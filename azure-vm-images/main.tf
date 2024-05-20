40abaefcc1b155eba8018d65cff68d365d258b05
suncoastcloud <ken.pinto@outlook.com>
2024-05-20 10:38:30 -0400

    add files for vm images

---
+++ azure-vm-images/main.tf
@@ -0,0 +1,68 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rhel-vm-rg"
+  location = "southcentralus"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = "machine"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_F2"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.nic.id] 
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "RedHat"
+    offer     = "RHEL"
+    sku       = "8_9"
+    version   = "latest"
+  }
+
+  # source_image_reference {
+  #   publisher = "redhat-rhel"
+  #   offer     = "rh-rhel"
+  #   sku       = "rh-rhel8-gen1"
+  #   version   = "latest"
+  # }
+
+  # plan {
+  #   publisher = "redhat-rhel"
+  #   product   = "rh-rhel"
+  #   name      = "rh-rhel8-gen1"
+  # }
+
+  computer_name                   = "hostname"
+  admin_password                  = "P@ssw0rd123!"
+  disable_password_authentication = false
+}
